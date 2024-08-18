8a9afffe4642a7219cf57c75fa7e9267ea921020
Alex Charles <alexscharles@gmail.com>
2024-08-18 15:04:31 -0500

    mods

---
+++ src/app/emulation/RewindBro.hpp
@@ -0,0 +1,18 @@
+//
+// Created by alexs on 8/18/2024.
+//
+
+#ifndef REWINDBRO_HPP
+#define REWINDBRO_HPP
+
+namespace firelight {
+namespace emu {
+
+class RewindBro {
+
+};
+
+} // emu
+} // firelight
+
+#endif //REWINDBRO_HPP
