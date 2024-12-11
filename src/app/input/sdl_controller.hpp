4b22cab06879eea5bf2d5b4fe193bf154388e7f2
Alex Charles <alexscharles@gmail.com>
2024-12-11 12:19:36 -0600

    ehh

---
+++ src/app/input/sdl_controller.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <QObject>
+#include <QQuickItem>
+
+#include "firelight/libretro/retropad.hpp"
+#include <SDL_gamecontroller.h>
+#include <string>
+
+#include "profiles/controller_profile.hpp"
+#include "gamepad_type.hpp"
+
+namespace firelight::Input {
+  class SdlController final : public libretro::IRetroPad {
+  public:
+    SdlController(SDL_GameController *t_controller, int32_t t_joystickIndex);
+
+    ~SdlController() override;
+
+    void setControllerProfile(const std::shared_ptr<input::ControllerProfile> &profile);
+
+    bool isButtonPressed(int platformId, Input t_button) override;
+
+    int16_t getLeftStickXPosition(int platformId) override;
+
+    int16_t getLeftStickYPosition(int platformId) override;
+
+    int16_t getRightStickXPosition(int platformId) override;
+
+    int16_t getRightStickYPosition(int platformId) override;
+
+    [[nodiscard]] int32_t getInstanceId() const;
+
+    [[nodiscard]] int32_t getDeviceIndex() const;
+
+    [[nodiscard]] std::string getControllerName() const;
+
+    void setPlayerIndex(int t_newPlayerIndex) const;
+
+    [[nodiscard]] int getPlayerIndex() const;
+
+    void setStrongRumble(int platformId, uint16_t t_strength) override;
+
+    void setWeakRumble(int platformId, uint16_t t_strength) override;
+
+    [[nodiscard]] bool isWired() const;
+
+    GamepadType getGamepadType() const;
+
+  private:
+    static std::map<int, int> controllerTypeMap;
+
+    std::shared_ptr<input::ControllerProfile> m_profile = nullptr;
+
+    SDL_GameController *m_SDLController = nullptr;
+    SDL_Joystick *m_SDLJoystick = nullptr;
+    int32_t m_SDLJoystickDeviceIndex = -1;
+    int32_t m_SDLJoystickInstanceId = -1;
+  };
+} // namespace firelight::Input
