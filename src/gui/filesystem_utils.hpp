a2c60ffd20ad20667e60ef6efd6b90c0bad2d3e2
aquova <mail@aquova.net>
2025-08-16 22:25:44 -0500

    Use helper functions for all file URI handling

--- src/gui/filesystem_utils.hpp
+++ src/gui/filesystem_utils.hpp
@@ -6,10 +6,10 @@ class FilesystemUtils : public QObject {
   Q_OBJECT
 
 public:
-  Q_INVOKABLE QString getFileURI();
-  Q_INVOKABLE bool isFile(const QString &path);
-  Q_INVOKABLE QString prependFileURI(const QString &path);
-  Q_INVOKABLE QString removeFileURI(const QString &path);
+  Q_INVOKABLE static QString getFileURI();
+  Q_INVOKABLE static bool isFile(const QString &path);
+  Q_INVOKABLE static QString prependFileURI(const QString &path);
+  Q_INVOKABLE static QString removeFileURI(const QString &path);
   Q_INVOKABLE bool showInFilesystem(const QString &path);
 };
 } // namespace firelight::gui
