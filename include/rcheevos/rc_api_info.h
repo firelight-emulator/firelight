244f0029c4e9d86c81431157a8bd8c2b83b3cab9
biscuitcakes <alexscharles@gmail.com>
2026-09-22 21:19:50 -0500

    Update rcheevos includes

--- include/rcheevos/rc_api_info.h
+++ include/rcheevos/rc_api_info.h
@@ -33,6 +33,8 @@ rc_api_fetch_achievement_info_request_t;
 typedef struct rc_api_achievement_awarded_entry_t {
   /* The user associated to the entry */
   const char* username;
+  /* A URL to the user's avatar image */
+  const char* avatar_url;
   /* When the achievement was awarded */
   time_t awarded;
 }
@@ -62,6 +64,7 @@ typedef struct rc_api_fetch_achievement_info_response_t {
 rc_api_fetch_achievement_info_response_t;
 
 RC_EXPORT int RC_CCONV rc_api_init_fetch_achievement_info_request(rc_api_request_t* request, const rc_api_fetch_achievement_info_request_t* api_params);
+RC_EXPORT int RC_CCONV rc_api_init_fetch_achievement_info_request_hosted(rc_api_request_t* request, const rc_api_fetch_achievement_info_request_t* api_params, const rc_api_host_t* host);
 /* [deprecated] use rc_api_process_fetch_achievement_info_server_response instead */
 RC_EXPORT int RC_CCONV rc_api_process_fetch_achievement_info_response(rc_api_fetch_achievement_info_response_t* response, const char* server_response);
 RC_EXPORT int RC_CCONV rc_api_process_fetch_achievement_info_server_response(rc_api_fetch_achievement_info_response_t* response, const rc_api_server_response_t* server_response);
@@ -88,6 +91,8 @@ rc_api_fetch_leaderboard_info_request_t;
 typedef struct rc_api_lboard_info_entry_t {
   /* The user associated to the entry */
   const char* username;
+  /* A URL to the user's avatar image */
+  const char* avatar_url;
   /* The rank of the entry */
   uint32_t rank;
   /* The index of the entry */
@@ -129,12 +134,16 @@ typedef struct rc_api_fetch_leaderboard_info_response_t {
   /* The number of items in the entries array */
   uint32_t num_entries;
 
+  /* The total number of entries on the server */
+  uint32_t total_entries;
+
   /* Common server-provided response information */
   rc_api_response_t response;
 }
 rc_api_fetch_leaderboard_info_response_t;
 
 RC_EXPORT int RC_CCONV rc_api_init_fetch_leaderboard_info_request(rc_api_request_t* request, const rc_api_fetch_leaderboard_info_request_t* api_params);
+RC_EXPORT int RC_CCONV rc_api_init_fetch_leaderboard_info_request_hosted(rc_api_request_t* request, const rc_api_fetch_leaderboard_info_request_t* api_params, const rc_api_host_t* host);
 /* [deprecated] use rc_api_process_fetch_leaderboard_info_server_response instead */
 RC_EXPORT int RC_CCONV rc_api_process_fetch_leaderboard_info_response(rc_api_fetch_leaderboard_info_response_t* response, const char* server_response);
 RC_EXPORT int RC_CCONV rc_api_process_fetch_leaderboard_info_server_response(rc_api_fetch_leaderboard_info_response_t* response, const rc_api_server_response_t* server_response);
@@ -175,10 +184,96 @@ typedef struct rc_api_fetch_games_list_response_t {
 rc_api_fetch_games_list_response_t;
 
 RC_EXPORT int RC_CCONV rc_api_init_fetch_games_list_request(rc_api_request_t* request, const rc_api_fetch_games_list_request_t* api_params);
+RC_EXPORT int RC_CCONV rc_api_init_fetch_games_list_request_hosted(rc_api_request_t* request, const rc_api_fetch_games_list_request_t* api_params, const rc_api_host_t* host);
 /* [deprecated] use rc_api_process_fetch_games_list_server_response instead */
 RC_EXPORT int RC_CCONV rc_api_process_fetch_games_list_response(rc_api_fetch_games_list_response_t* response, const char* server_response);
 RC_EXPORT int RC_CCONV rc_api_process_fetch_games_list_server_response(rc_api_fetch_games_list_response_t* response, const rc_api_server_response_t* server_response);
 RC_EXPORT void RC_CCONV rc_api_destroy_fetch_games_list_response(rc_api_fetch_games_list_response_t* response);
+
+/* --- Fetch Game Titles --- */
+
+/**
+ * API parameters for a fetch games list request.
+ */
+typedef struct rc_api_fetch_game_titles_request_t {
+  /* An array of game ids to fetch titles for */
+  const uint32_t* game_ids;
+  /* The number of items in the game_ids array */
+  uint32_t num_game_ids;
+}
+rc_api_fetch_game_titles_request_t;
+
+/* A game title entry */
+typedef struct rc_api_game_title_entry_t {
+  /* The unique identifier of the game */
+  uint32_t id;
+  /* The title of the game */
+  const char* title;
+  /* The image name for the game badge */
+  const char* image_name;
+  /* The URL for the game badge image */
+  const char* image_url;
+}
+rc_api_game_title_entry_t;
+
+/**
+ * Response data for a fetch games title request.
+ */
+typedef struct rc_api_fetch_game_titles_response_t {
+  /* An array of requested entries */
+  rc_api_game_title_entry_t* entries;
+  /* The number of items in the entries array */
+  uint32_t num_entries;
+
+  /* Common server-provided response information */
+  rc_api_response_t response;
+}
+rc_api_fetch_game_titles_response_t;
+
+RC_EXPORT int RC_CCONV rc_api_init_fetch_game_titles_request(rc_api_request_t* request, const rc_api_fetch_game_titles_request_t* api_params);
+RC_EXPORT int RC_CCONV rc_api_init_fetch_game_titles_request_hosted(rc_api_request_t* request, const rc_api_fetch_game_titles_request_t* api_params, const rc_api_host_t* host);
+RC_EXPORT int RC_CCONV rc_api_process_fetch_game_titles_server_response(rc_api_fetch_game_titles_response_t* response, const rc_api_server_response_t* server_response);
+RC_EXPORT void RC_CCONV rc_api_destroy_fetch_game_titles_response(rc_api_fetch_game_titles_response_t* response);
+
+/* --- Fetch Game Hashes --- */
+
+/**
+ * API parameters for a fetch games list request.
+ */
+typedef struct rc_api_fetch_hash_library_request_t {
+  /**
+   * The unique identifier of the console to query.
+   * Passing RC_CONSOLE_UNKNOWN will return hashes for all consoles.
+   */
+  uint32_t console_id;
+} rc_api_fetch_hash_library_request_t;
+
+/* A hash library entry */
+typedef struct rc_api_hash_library_entry_t {
+  /* The hash for the game */
+  const char* hash;
+  /* The unique identifier of the game */
+  uint32_t game_id;
+} rc_api_hash_library_entry_t;
+
+/**
+ * Response data for a fetch hash library request.
+ */
+typedef struct rc_api_fetch_hash_library_response_t {
+  /* An array of entries, one per game */
+  rc_api_hash_library_entry_t* entries;
+  /* The number of items in the entries array */
+  uint32_t num_entries;
+
+  /* Common server-provided response information */
+  rc_api_response_t response;
+}
+rc_api_fetch_hash_library_response_t;
+
+RC_EXPORT int RC_CCONV rc_api_init_fetch_hash_library_request(rc_api_request_t* request, const rc_api_fetch_hash_library_request_t* api_params);
+RC_EXPORT int RC_CCONV rc_api_init_fetch_hash_library_request_hosted(rc_api_request_t* request, const rc_api_fetch_hash_library_request_t* api_params, const rc_api_host_t* host);
+RC_EXPORT int RC_CCONV rc_api_process_fetch_hash_library_server_response(rc_api_fetch_hash_library_response_t* response, const rc_api_server_response_t* server_response);
+RC_EXPORT void RC_CCONV rc_api_destroy_fetch_hash_library_response(rc_api_fetch_hash_library_response_t* response);
 
 RC_END_C_DECLS
 
